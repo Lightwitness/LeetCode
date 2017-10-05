c81f08d45ae19c641d55cea8cfb60c33666e378c
Lightwitness <498969498@qq.com>
2017-10-05 11:20:18 +0800

    AC

--- 657. Judge Route Circle/JudgeRouteCircle.py
+++ 657. Judge Route Circle/JudgeRouteCircle.py
@@ -5,19 +5,18 @@
 @author: Lightwitness
 @contact: 498969498@qq.com
 @software: PyCharm
-@file: HammingDistance.py
-@time: 2017/10/5 10:53
+@file: JudgeRouteCircle.py
+@time: 2017/10/5 11:18
 """
+import collections
 class Solution(object):
     def judgeCircle(self, moves):
         """
         :type moves: str
         :rtype: bool
         """
-        d = {}
-        for k in moves:
-            d[k]+=1
-        if (d['R']==d['L']&& d['U']==d['D']):
-            return true
+        d=collections.Counter(moves)
+        if (d['R']==d['L'])and(d['U']==d['D']):
+            return True
         else:
-            return false
+            return False
